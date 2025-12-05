e7c7b06b1d55b455ac2e506283087c6d3610246d
FranklinMonro <fbg2468@gmail.com>
2025-12-05 03:30:20 +0000

    add airbnb eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: ['prettier'],
+  extends: [
+    'airbnb-base',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'no-console': 'warn',
+    'no-unused-vars': 'warn',
+  },
+};
