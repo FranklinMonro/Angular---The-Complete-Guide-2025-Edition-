94cdb7dc7d2276fc683c3199d9be6b7d9c62a33e
FranklinMonro <fbg2468@gmail.com>
2025-12-05 04:57:47 +0200

    restart repository

--- 1_install_web.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source 0_variables_for_scripts.sh
-
-echo $printAction Install Angular CLI Globaly in .devontainer $princtClear
-npm install -g @angular/cli
-
-echo $printDirectory "$PWD" $princtClear
