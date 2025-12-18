6652d94602ce79baf61897aee4cd37be913b9481
FranklinMonro <fbg2468@gmail.com>
2025-12-18 08:56:55 +0000

    section_2_video_39

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { Header } from './header/header';
 import { User } from './user/user';
 import { DUMMY_USERS } from './user/dummy-users';
+import { Tasks } from './tasks/tasks';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, User],
+  imports: [Header, User, Tasks],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
@@ -14,7 +15,13 @@ export class App {
 
   users = DUMMY_USERS;
 
+  selectedUserID = this.users[0].id;
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserID);
+  }
+
   onSelectUser(selectedID: string) {
-    console.log(`User selected: ${selectedID}`);
+    this.selectedUserID = selectedID;
   }
 }
