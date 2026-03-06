a51b636aea772be0e03ee08bb1d981b4280f04c6
FranklinMonro <fbg2468@gmail.com>
2026-03-06 04:33:29 +0000

    section_6_video_107

--- src/app/sup-app/sup-dashboard/dashboard-item/item-tickets/new-ticket/new-ticket.ts
+++ src/app/sup-app/sup-dashboard/dashboard-item/item-tickets/new-ticket/new-ticket.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SharedButtonComponent } from '../../../../shared/button/button';
+import { ControlComponent } from '../../../../shared/control/control';
 
 @Component({
   selector: 'new-ticket',
-  imports: [SharedButtonComponent],
+  imports: [SharedButtonComponent, ControlComponent],
   templateUrl: './new-ticket.html',
   styleUrls: ['../../../../sup-app.css', './new-ticket.css'],
 })
