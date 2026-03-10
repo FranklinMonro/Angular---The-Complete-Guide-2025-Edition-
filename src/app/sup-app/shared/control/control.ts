a7ff680e6cfa2d976791702bc8210658ff1e45ca
FranklinMonro <fbg2468@gmail.com>
2026-03-10 05:14:03 +0000

    section_6_video_118

--- src/app/sup-app/shared/control/control.ts
+++ src/app/sup-app/shared/control/control.ts
@@ -6,7 +6,15 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   templateUrl: './control.html',
   styleUrl: './control.css',
   encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control',
+    '(click)': 'onClick()',
+  },
 })
 export class ControlComponent {
   label = input.required<string>();
+
+  onClick(): void {
+    console.log('Control clicked');
+  }
 }
